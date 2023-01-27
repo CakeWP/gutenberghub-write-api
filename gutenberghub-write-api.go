9a7ffafa4962d0ed5b9c0994b2f99a690a2bde79
Zafar Kamal <sk4915497@gmail.com>
2023-01-27 17:47:33 +0500

    :100: remove rate-limiter

--- gutenberghub-write-api.go
+++ gutenberghub-write-api.go
@@ -4,54 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
 	"reflect"
 	"strings"
 
 	"github.com/pocketbase/pocketbase"
 
-	"github.com/labstack/echo/v5"
-	"github.com/labstack/echo/v5/middleware"
 	"github.com/pocketbase/pocketbase/core"
 )
-
-// RateLimitCollectionOperations is a middleware that rate limits HTTP requests on collection operations.
-// Possible operations:
-// - list
-// - view
-// - create
-// - update
-// - delete
-func RateLimitCollectionOperations(collection string, operations []string, limiter echo.MiddlewareFunc) echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			contains := func(s string, list []string) bool {
-				for _, v := range list {
-					if v == s {
-						return true
-					}
-				}
-				return false
-			}
-
-			collectionEndpoint := fmt.Sprintf("/api/collections/%s/records", collection)
-
-			if contains("list", operations) && c.Request().URL.Path == collectionEndpoint && c.Request().Method == http.MethodGet {
-				return limiter(next)(c)
-			} else if contains("view", operations) && strings.HasPrefix(c.Request().URL.Path, collectionEndpoint) && c.Request().Method == http.MethodGet {
-				return limiter(next)(c)
-			} else if contains("create", operations) && strings.HasPrefix(c.Request().URL.Path, collectionEndpoint) && c.Request().Method == http.MethodPost {
-				return limiter(next)(c)
-			} else if contains("update", operations) && strings.HasPrefix(c.Request().URL.Path, collectionEndpoint) && c.Request().Method == http.MethodPatch {
-				return limiter(next)(c)
-			} else if contains("delete", operations) && strings.HasPrefix(c.Request().URL.Path, collectionEndpoint) && c.Request().Method == http.MethodDelete {
-				return limiter(next)(c)
-			}
-
-			return next(c)
-		}
-	}
-}
 
 func excludeFields(data interface{}, fields []string) ([]byte, error) {
 	// check if data is of type array
@@ -90,15 +49,6 @@ func excludeFields(data interface{}, fields []string) ([]byte, error) {
 
 func main() {
 	app := pocketbase.New()
-
-	// Rate limiter.
-	limiter := middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(2))
-
-	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		// Rate limit list and view operations on the "posts" collection
-		e.Router.Use(RateLimitCollectionOperations("posts", []string{"list", "view"}, limiter))
-		return nil
-	})
 
 	// Adding a new query field parameter when listing view.
 	app.OnRecordsListRequest().Add(func(e *core.RecordsListEvent) error {
